e0256a7a3b800734759df32d31ff59116e071fb6
Janik Schumacher <rndmb0t@gmail.com>
2022-07-14 17:46:22 +0200

    test code improved

--- src/test.ts
+++ src/test.ts
@@ -1,3 +1,17 @@
 import { CircleParticle } from './particles/circle.js';
 
-CircleParticle.draw({ x: 100, y: 100, radius: 10, color: 'red', movement: 'xy', velocityY: 2, velocityX: 1 });
+CircleParticle.draw({ x: 300, y: 300, radius: 10, color: 'red', movement: 'angle', angle: 90, velocity: -5, acceleration: 0.1 });
+
+for (let i = 0; i < 5; i++) {
+  CircleParticle.draw({
+    x: 200,
+    y: 300,
+    radius: 10,
+    color: 'blue',
+    movement: 'xy',
+    velocityX: Math.random() - 0.5,
+    velocityY: -(Math.random() * 2 + 5),
+    rotationY: 4,
+    gravity: 0.1
+  });
+}
