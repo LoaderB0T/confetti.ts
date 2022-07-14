00f23c61a8498f1b95d30130517af28e437b499f
Janik Schumacher <rndmb0t@gmail.com>
2022-07-14 14:19:45 +0200

    dummy test

---
+++ test/tests/main.test.ts
@@ -0,0 +1,22 @@
+import { Confetti } from '../../src/confetti.js';
+import { randomStub, resetRandomStub } from '../random-stub.js';
+
+describe('', () => {
+  let confetti: Confetti;
+  let originalRandom: typeof Math.random;
+  beforeEach(() => {
+    originalRandom = Math.random;
+    resetRandomStub();
+    Math.random = () => randomStub();
+  });
+  afterEach(() => {
+    Math.random = originalRandom;
+  });
+
+  test('random mock works', async () => {
+    expect(Math.random()).toBe(0.908788990863389);
+    expect(Math.random()).toBe(0.7982002517390028);
+    expect(Math.random()).toBe(0.4753399903420179);
+    expect(Math.random()).toBe(0.00016161331579800375);
+  });
+});
